f23093e15fc701b8667d50ede16561769c5ba322
REAL <cwma.campbell@gmail.com>
2021-08-16 15:02:38 +0000

    added basename command options

--- video2audio.sh
+++ video2audio.sh
@@ -40,3 +40,27 @@ done
 # rm *.webm
 
 # rm *.m4a
+
+########################################
+# ffmpeg opus to ogg from webm youtube #
+########################################
+
+
+# .webm files can contain vorbis audio, but it can also contain opus audio
+# Also an ogg file can contain both audio formats
+# you can transfer the ogg audio from the video without the need to convert to an .ogg file
+
+# ffmpeg -i "$INPUTFILE" -vn -c:a copy "$(basename "$INPUTFILE" .webm)".ogg
+
+# If you need vorbis audio in an ogg container, but your .webm contains opus audio, then you need to transcode to ogg
+
+# In oggenc, a -q 6 setting will result in a file with variable bitrate at about 192 kbps
+# This is a setting where quality loss mostly will not be perceived anymore ("transparent")
+# and is a setting optimal to achieve excellent quality at a minimum file size
+
+# In ffmpeg, the corresponding option is -qscale:a 6:
+
+# ffmpeg -i "$INPUTFILE" -vn -c:a libvorbis -qscale:a 6 "$(basename "$INPUTFILE" .webm)".ogg
+
+# The range is -1 to 10, where 10 is very high quality
+# The default is -qscale:a 3, which corresponds to an average bitrate of about 112
